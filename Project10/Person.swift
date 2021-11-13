4ef070fcb7f9046d07f40fe182ed77b50053cafe
maiconlmotadev <maiconlmotadev@hotmail.com>
2021-11-13 17:12:03 +0000

    Lançamento de projeto: opção de resolução A

---
+++ Project10/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Maicon Mota on 03/11/2021.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+
+    
+}
